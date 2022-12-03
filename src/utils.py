b9facf237a5f62c3ec1e9a98da946c27bf6b8a85
Soogeun Park <bwmelon97@naver.com>
2022-12-03 22:41:27 +0900

    feat: add color constants in the utils

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,11 @@
+from enum import Enum
+
 WINDOW_SIZE = 800
+
+class Color(Enum):
+    WHITE   = [1, 1, 1]
+    RED     = [255/256, 158/256, 158/256]  # rgb(255, 158, 158)
+    GREEN   = [192/256, 238/256, 228/256]   # rgb(192, 238, 228)
+    YELLOW  = [248/256, 249/256, 136/256]   # rgb(248, 249, 136)
+    MAGENTA = [1, 0, 1]
+    GREY    = [0.4, 0.4, 0.4]
